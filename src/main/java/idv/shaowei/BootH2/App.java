00231eaeea06361e31905857a629e0cca48d38ce
hongshaowei <hongshaowei2015@gmail.com>
2019-08-27 11:32:41 +0800

    create App

---
+++ src/main/java/idv/shaowei/BootH2/App.java
@@ -0,0 +1,13 @@
+package idv.shaowei.BootH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import idv.shaowei.BootH2.App;
+
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+    	SpringApplication.run(App.class, args);
+    }
+}
